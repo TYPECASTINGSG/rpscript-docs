e8450d24d45b6e3c66c0c217c4f6f59b6a79b60a
James Chong <wei3hua2@gmail.com>
2018-08-19 10:21:56 +0800

    update api docs

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@
 # cd ../rpscript-api-beeper && npm run build  #COMPLETED
 # cd ../rpscript-api-file && npm run build
 # cd ../rpscript-api-hogan && npm run build #COMPLETED
-# cd ../rpscript-api-date-fp && npm run build
-cd ../rpscript-api-adm-zip && npm run build #stable:MED
+cd ../rpscript-api-date-fp && npm run build
+# cd ../rpscript-api-adm-zip && npm run build #stable:MED
 # cd ../rpscript-api-notifier && npm run build #COMPLETED
 
 # cd ../rpscript-api-columnify && npm run build  #stable:HIGH
